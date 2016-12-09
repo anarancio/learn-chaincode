f5f31e539a93852260c93c320803a4a5974e659f
Alejandro Narancio <ale.narancio@gmail.com>
2016-12-09 13:02:09 -0300

    test 2

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -69,6 +69,9 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	if function == "dummy_query" {											//read a variable
 		fmt.Println("hi there " + function)						//error
 		return nil, nil;
+	} else if function == "test_query" {
+		fmt.Println("TEST QUERY");
+		return nil, nil;
 	}
 	fmt.Println("query did not find func: " + function)						//error
 
