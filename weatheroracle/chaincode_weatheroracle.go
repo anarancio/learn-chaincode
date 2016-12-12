706e7be0173858db3a97c64b12c1c07d054b64d8
Alejandro Narancio <ale.narancio@gmail.com>
2016-12-12 14:47:57 -0300

    changing invoked function on oracle weather init

--- weatheroracle/chaincode_weatheroracle.go
+++ weatheroracle/chaincode_weatheroracle.go
@@ -28,7 +28,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
   params = append(params, stub.GetTxID())
   params = append(params, "")
 
-  f := "invoke"
+  f := "add_weather_oracle"
   invokeArgs := util.ToChaincodeArgs(f, stub.GetTxID(), "Temperatura en Montevideo en F")
   stub.InvokeChaincode(args[0], invokeArgs)
 
