237444c5b68aa94bdcbc15b1b3ce34b5e4bef531
Alejandro Narancio <ale.narancio@gmail.com>
2016-12-12 16:34:48 -0300

    adding add_listener, new_temp and get_temperature to weather oracle

--- weatheroracle/chaincode_weatheroracle.go
+++ weatheroracle/chaincode_weatheroracle.go
@@ -39,15 +39,81 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 
 	stub.PutState("oracle_listeners", bytes)
 
+  temp := "No disponible"
+  bytes, err = json.Marshal(temp)
+	if err != nil { return nil, errors.New("Error creating temp") }
+  stub.PutState("temperature", bytes)
+
   return nil, nil
 }
 
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
   fmt.Println("invoke is running " + function)
+  if function == "add_listener" {
+    return t.add_listener(stub, args)
+  } else if function == "new_temp" {
+    return t.new_temp(stub, args)
+  }
   return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+func (t *SimpleChaincode) new_temp(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+  fmt.Println("new_temp called")
+  if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1 (new temp)")
+	}
+
+  temp := args[0]
+  bytes, err := json.Marshal(temp)
+	if err != nil { return nil, errors.New("Error updating temp") }
+  stub.PutState("temperature", bytes)
+
+  var listeners []string
+  bytes, err = stub.GetState("oracle_listeners")
+  err = json.Unmarshal(bytes, &listeners)
+  for i := 0; i<len(listeners); i++ {
+    f := "new_temp"
+    invokeArgs := util.ToChaincodeArgs(f, temp)
+    stub.InvokeChaincode(listeners[i], invokeArgs)
+  }
+
+  return nil, nil
+}
+
+func (t *SimpleChaincode) add_listener(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+  fmt.Println("add_listener called")
+  if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1 (listener uuid)")
+	}
+
+  bytes, err := stub.GetState("oracle_listeners")
+	if err != nil { return nil, errors.New("Error getting oracle_listeners state") }
+	var listeners []string
+	err = json.Unmarshal(bytes, &listeners)
+
+  // create the record for the produce: <UUID>,<type>,<description
+	listeners = append(listeners, args[0])
+	bytes, err = json.Marshal(listeners)
+	if err != nil { return nil, errors.New("Error creating oracle listeners") }
+	stub.PutState("oracle_listeners", bytes)
+
+	return nil, nil
 }
 
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
   fmt.Println("query is running " + function)
+  if function == "get_temperature" {
+    return t.get_temperature(stub)
+  }
   return nil, errors.New("Received unknown function query: " + function)
+}
+
+func (t *SimpleChaincode) get_temperature(stub shim.ChaincodeStubInterface) ([]byte, error) {
+  fmt.Println("get_temperature called")
+	bytes, err := stub.GetState("temperature")
+	fmt.Println(bytes)
+
+	if err != nil { return nil, errors.New("Error getting temperature record") }
+
+	return bytes, nil
 }
